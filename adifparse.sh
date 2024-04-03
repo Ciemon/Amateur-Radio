0398c90d12f4111eb91777b2725a2cef69dfe487
Ciemon <ciemon@gmail.com>
2024-04-03 21:22:58 +0100

    Update adifparse.sh
    Added a couple of lines of description

--- adifparse.sh
+++ adifparse.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 #Get the number of QSOs and unique DXCCs from and adif file
 #
+#My logbook outputs to the file named logbook_export.adi which can be changed if your filename is different.
+#This sript file needs to be made executable for it to run, and will output to the terminal
+#
 qso=$(grep -c CALL: logbook_export.adi)
 dxcc=$(sort -u logbook_export.adi | grep -c "<DXCC:")
 echo "Number of QSOs=" $qso
